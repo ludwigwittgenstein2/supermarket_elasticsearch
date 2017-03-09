c65634119a0548b5dd81c6975658e71dfbd6f787
Harshil <harshil.rastogi@innovaccer.com>
2017-03-10 01:04:26 +0530

    top product

---
+++ top_product.py
@@ -0,0 +1,38 @@
+import requests
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+
+products = requests.post(
+    'http://localhost:9200/_sql', data='SELECT sum(SALES_VALUE) FROM transactions group by PRODUCT_ID order by sum(SALES_VALUE) desc limit 10').json()
+
+print 'name, quantity, value'
+
+names = []
+values = []
+
+N = 10
+
+for product in products['aggregations']['PRODUCT_ID']['buckets']:
+    product_code = product['key']
+    quantity = product['doc_count']
+    value = product['SUM(SALES_VALUE)']['value']
+    name_json = requests.post('http://localhost:9200/_sql',
+                              data='select SUB_COMMODITY_DESC from product where PRODUCT_ID = ' + str(product_code)).json()
+
+    name = name_json['hits']['hits'][0]['_source']['SUB_COMMODITY_DESC']
+
+    print name, quantity, value
+
+    names.append(name)
+    values.append(value)
+
+ind = np.arange(N)
+print ind, 'np.arange(N)'
+width = 0.35
+
+fig, ax = plt.subplots()
+rects1 = ax.bar(ind, values, width)
+ax.set_xticklabels(names)
+
+plt.show()
